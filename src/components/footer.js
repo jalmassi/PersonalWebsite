bfefb5ac37fc71b5043f8bfa2aa40656ce350a75
jalmassi <justin.almassi@gmail.com>
2021-12-04 14:48:03 -0500

    git footer link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Icon } from '@components/icons';
-import { socialMedia } from '@config';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { Icon } from "@components/icons";
+import { socialMedia } from "@config";
 
 const StyledFooter = styled.footer`
   ${({ theme }) => theme.mixins.flexCenter};
@@ -74,19 +74,19 @@ const Footer = () => {
   });
 
   useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV !== "production") {
       return;
     }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
+    fetch("https://api.github.com/repos/jalmassi")
+      .then((response) => response.json())
+      .then((json) => {
         const { stargazers_count, forks_count } = json;
         setGitHubInfo({
           stars: stargazers_count,
           forks: forks_count,
         });
       })
-      .catch(e => console.error(e));
+      .catch((e) => console.error(e));
   }, []);
 
   return (
@@ -109,7 +109,7 @@ const Footer = () => {
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
               <span>
-                <Icon name="Star" />
+                {/* <Icon name="Star" /> */}
                 <span>{githubInfo.stars.toLocaleString()}</span>
               </span>
             </div>
